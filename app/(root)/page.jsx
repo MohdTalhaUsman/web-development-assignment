7b63b0dc0182d90df35e5930ada6f9f2a5c2ac12
MohdTalhaUsman <talhausman347@gmail.com>
2025-09-02 20:16:41 +0530

    adding tool tip

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -5,8 +5,10 @@ import SchoolCard from "../components/SchoolCard";
 
 export default function Home() {
   const [schoolsArray, setSchoolsArray] = useState([]);
+  const [isFetchingData, setIsFetchingData] = useState(true);
 
   useEffect(() => {
+    setIsFetchingData(true);
     (async () => {
       try {
         const fetchResponse = await fetch("/api/schools/");
@@ -17,6 +19,8 @@ export default function Home() {
         setSchoolsArray(schoolsData);
       } catch (error) {
         console.log(error.message);
+      } finally {
+        setIsFetchingData(false);
       }
     })();
   }, []);
@@ -24,6 +28,15 @@ export default function Home() {
   return (
     <>
       <div className="p-[4em]">
+        {!isFetchingData && schoolsArray.length === 0 && (
+          <h2 className="text-[var(--heading-1-color)] text-[3.6em] font-sans font-bold text-center">
+            No school data found.
+            <br />
+            <span className="text-[0.6em] font-mono font-normal text-black">
+              Click Add School to add school data.
+            </span>
+          </h2>
+        )}
         <ul className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-x-[64px]  gap-y-[32px] justify-items-center">
           {schoolsArray.map((schoolObject) => (
             <SchoolCard key={schoolObject.id} schoolObject={schoolObject} />
